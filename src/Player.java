d63e976d815c5354be7da4a5127a05cd216c414a
grimauce <grimauce@PC-A213-01.iut-valence.fr>
2016-04-07 11:13:14 +0200

    The class Player has been changed because it doesn't define the color of the player anymore.

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,3 @@
-// TODO fix comment (ask for advice)
 /**
  * Player class with his constructor.
  * This represents a player.
@@ -7,34 +6,5 @@
  */
 public class Player
 {
-		/**
-		 * Give a color to the player's pawns
-		 * 0 --> white
-		 * 1 --> black
-		 */
-		private boolean color;
-		
-		
-		
-		/**
-		 * Creates a new player, in starting configuration, ready to play
-		 */	
-		public Player()
-		{
-			
-			this.player = DEFAULT_PLAYER;
-			
-		}
-		
-		
-		
-		/**
-		 * Change the player
-		 */
-	public switchPlayer()
-		{
-			this.color = !this.color;
-		}
-
 	
 }
